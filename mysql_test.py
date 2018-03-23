26ff1bfc0eddd7412d351ed422242fe32f9f21ed
jerry <614237720@qq.com>
2018-03-23 11:56:24 +0800

    标注一下数据库连接的demo
    标注一下数据库连接的demo

--- mysql_test.py
+++ mysql_test.py
@@ -4,14 +4,21 @@
 import pymysql
 import time
 
+# 数据库中文乱码需要设置charset字段在connect过程中。
+
 try:
-    conn = pymysql.connect(host='localhost', user='root', passwd='asdf', db='test', port=3306)
-    cur = conn.cursor()
-    cur.execute('select * from orders')
+    db = pymysql.connect(host='localhost', user='root', passwd='asdf', db='test', port=3306)
+# 获取游标
+    cur = db.cursor()
+    sql = 'select * from orders'
+    cur.execute(sql)
+# 合并一下SQL
+    db.commit() 
     kk =list(cur)
     for j in range(0, len(kk)):
         print kk[j]
     cur.close()
-    conn.close()
+    db.close()
+# 捕获一下数据库的错误
 except pymysql.Error, e:
     print 'mysql error %d: %s' %(e.args[0], e.args[1])
