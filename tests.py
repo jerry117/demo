f06a215ec94718c2ce119aa5e0afffc53aa9c5b0
lijialiang <lijialiang@guorou.tv>
2021-09-14 14:16:25 +0800

    test

--- tests.py
+++ tests.py
@@ -1,34 +1,51 @@
-# coding = utf-8
+# # coding = utf-8
 
-import requests
-from my_calendar import get_holidays
-from requests.exceptions import Timeout
-from unittest.mock import patch
+# import requests
+# from my_calendar import get_holidays
+# from requests.exceptions import Timeout
+# from unittest.mock import patch
+# import unittest
+
+# class TestCalendar(unittest.TestCase):
+#     @patch('my_calendar.requests')
+#     def test_get_holidays_timeout(self, mock_requests):
+#         mock_requests.get.side_effect = Timeout
+#         with self.assertRaises(Timeout):
+#             get_holidays()
+#             mock_requests.get.assert_called_once()
+
+#     # mock对象的一部分，使用patch()作为上下文管理器，可以用with语句。
+#     # def test_get_holidays_timeout(self):
+#     #     with patc('my_calendar.requests') as mock_requests:
+#     #         mock_requests.get.side_effect = Timeout
+#     #         with self.assertRaises(Timeout):
+#     #             get_holidays()
+#     #             mock_requests.get.assert_called_once()
+
+
+#     # 只是mock了get方法而不是整个requests包，其他属性还是与原来相同
+#     # @patch.object(requests, 'get', side_effect=requests.exceptions.Timeout)
+#     # def test_get_holidays_timeout(self, mock_requests):
+#     #     with self.assertRaises(requests.exceptions.Timeout):
+#     #         get_holidays()
+
+
+# if __name__ == "__main__":
+#     unittest.main()
+
+
+
 import unittest
 
-class TestCalendar(unittest.TestCase):
-    @patch('my_calendar.requests')
-    def test_get_holidays_timeout(self, mock_requests):
-        mock_requests.get.side_effect = Timeout
-        with self.assertRaises(Timeout):
-            get_holidays()
-            mock_requests.get.assert_called_once()
+# A simple function to illustrate
+def parse_int(s):
+    return int(s)
 
-    # mock对象的一部分，使用patch()作为上下文管理器，可以用with语句。
-    # def test_get_holidays_timeout(self):
-    #     with patc('my_calendar.requests') as mock_requests:
-    #         mock_requests.get.side_effect = Timeout
-    #         with self.assertRaises(Timeout):
-    #             get_holidays()
-    #             mock_requests.get.assert_called_once()
+class TestConversion(unittest.TestCase):
+    def test_bad_int(self):
+        self.assertRaises(ValueError, parse_int, 'N/A')
 
 
-    # 只是mock了get方法而不是整个requests包，其他属性还是与原来相同
-    # @patch.object(requests, 'get', side_effect=requests.exceptions.Timeout)
-    # def test_get_holidays_timeout(self, mock_requests):
-    #     with self.assertRaises(requests.exceptions.Timeout):
-    #         get_holidays()
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_bad_int2(self):
+        self.assertRaisesRegex(ValueError, 'invalid literal .*',
+                                    parse_int, 'N/A')
